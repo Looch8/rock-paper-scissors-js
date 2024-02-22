99f025b9a9b5e5e24a78706c9d2b3f047bf59942
Luke Wheldale <lukewheldale@gmail.com>
2024-02-22 16:15:13 +1030

    feat: round of RPS playable

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 function getComputerChoice() {
 	// Generate random number
-	const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+	const getRandomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
-	if (randomNumber == 1) {
+	if (getRandomNumber == 1) {
 		return "Rock";
-	} else if (randomNumber == 2) {
+	} else if (getRandomNumber == 2) {
 		return "Paper";
 	} else {
 		return "Scissors";
 	}
 }
 
-console.log(getComputerChoice());
+// Get player Choice
+const getPlayerChoice = "Rock";
+
+function playRound(playerSelection, computerSelection) {
+	computerSelection = getComputerChoice().toLowerCase();
+	playerSelection = getPlayerChoice.toLowerCase();
+
+	// Draw conditions
+	if (computerSelection == playerSelection) {
+		return `DRAW! you both picked ${computerSelection}`;
+
+		// Computer winning conditions
+	} else if (
+		(computerSelection == "rock" && playerSelection == "scissors") ||
+		(computerSelection == "paper" && playerSelection == "rock") ||
+		(computerSelection == "scissors" && playerSelection == "paper")
+	) {
+		return `Computer WINS! ${computerSelection} beats ${playerSelection}`;
+	} else {
+		return `You WIN! ${playerSelection} beats ${computerSelection}`;
+	}
+}
+
+playRound();
