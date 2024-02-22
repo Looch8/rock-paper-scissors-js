330060eeba186893f9c0874b838e5397112f1508
Luke Wheldale <lukewheldale@gmail.com>
2024-02-22 15:42:10 +1030

    feat: return Rock, Paper, or Scissors in console

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function getComputerChoice() {
 	// Generate random number
 	const randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
-	return randomNumber;
+	if (randomNumber == 1) {
+		return "Rock";
+	} else if (randomNumber == 2) {
+		return "Paper";
+	} else {
+		return "Scissors";
+	}
 }
 
 console.log(getComputerChoice());
